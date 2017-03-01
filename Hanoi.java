c27c93814f634e0318d1dc2c02521fec46558d4f
younge2 <younge2@IT30154867.insttech.washington.edu>
2017-03-01 14:57:04 -0800

    Added Hanoi.java with stubs

---
+++ Hanoi.java
@@ -0,0 +1,26 @@
+
+public class Hanoi {
+	private Rod rod1;
+	private Rod rod2;
+	private Rod rod3;
+	
+	public Hanoi (int diskNum){
+		this.rod1 = new Rod('A', diskNum);
+		this.rod2 = new Rod('B', 0);
+		this.rod3 = new Rod('C', 0);
+		
+	}
+	
+	public void printStatus(){
+		
+	}
+	
+	public void move(){
+		
+	}
+	
+	public void recursiveTOH(){
+		
+	}
+
+}
