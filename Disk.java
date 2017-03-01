7a6ce16364222e0591d5c72d7e2b0686015bce0f
younge2 <younge2@IT30154867.insttech.washington.edu>
2017-03-01 13:47:35 -0800

    Started Hanoi project finished Disk

---
+++ Disk.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Disk {
+   private int size;
+   
+   
+   public Disk(int width){
+   this.size = width;
+   }
+   
+   public int getSize(){
+      return this.size;
+   }
+   public void setSize(int width){
+      this.size = width;
+   }
+
+}
